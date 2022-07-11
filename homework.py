85e56047aca5a33f8d2966af525a4ecc003a23a3
Kot_Barbarossa <kot_barba@MacBook-Pro-Kot-Barbarossa.local>
2022-07-12 00:24:28 +0300

    homework_bot 02 бот научился отправлять в чат статусы

--- homework.py
+++ homework.py
@@ -17,7 +17,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 600
+RETRY_TIME = 60
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -51,68 +51,85 @@ def say_no(update, context):
     }
     context.bot.send_message(chat_id=chat.id,text=answers.get(str(random_answer)))
 
+def check(update, context):
+    chat = update.effective_chat
+    # response = get_api_answer(1549962000)
+    # homework = response.json().get("homeworks")
+    # homework_status = homework[0].get("status")
+    # homework_name = homework[0].get("homework_name")
+    # context.bot.send_message(chat_id=chat.id,text=f'{Текущий статус проверки работы "{homework_name}": {homework_status}}')    
+    thirty_days = 2592000
+    current_timestamp = int(time.time()) - thirty_days    
+    response = get_api_answer(current_timestamp)
+    homework = check_response(response)
+    message = parse_status(homework)  
+    context.bot.send_message(chat_id=chat.id,text=f'{message}')
+
 def send_message(bot, message):
-    ...
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID,text=message)
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return response
 
 
 def check_response(response):
-
+    homeworks = response.json().get("homeworks")
+    homework = homeworks[0]
+    return homework
     ...
 
 
 def parse_status(homework):
     homework_name = homework.get("homework_name")
     homework_status = homework.get("status")
-    statuses = {
-        'reviewing': 'работа принята на проверку',
-        'approved': 'работа принята',
-        'rejected': 'работа возвращена для исправления ошибок'
-    }
-
-    verdict = statuses.get(homework_status)
+    verdict = HOMEWORK_STATUSES.get(homework_status)
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    flag = True
+    if PRACTICUM_TOKEN is None or TELEGRAM_TOKEN is None or TELEGRAM_CHAT_ID is None:
+        flag = False
+    return flag
 
 
 def main():
     """Основная логика работы бота."""
     UPADTER.dispatcher.add_handler(CommandHandler('start', wake_up))
+    UPADTER.dispatcher.add_handler(CommandHandler('check', check))
     UPADTER.dispatcher.add_handler(MessageHandler(Filters.text, say_no))
     UPADTER.start_polling()
-    UPADTER.idle() 
-    ...
+    # UPADTER.idle() 
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-    #twelve_hours = 43200
-    ...
+    twelve_hours = 43200
+    thirty_days = 2592000
+    current_timestamp = int(time.time()) - thirty_days
 
+    check_tokens()
     while True:
         try:
-            response = ...
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)
+            message = parse_status(homework)
+            send_message(bot, message)
 
-            ...
-
-            current_timestamp = ...
-            time.sleep(RETRY_TIME)
+            #current_timestamp = ...
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
+            send_message(bot, message)
+            # time.sleep(RETRY_TIME)
+        # else:
+        #     ...
+
+        time.sleep(RETRY_TIME)
+        
 
 
 if __name__ == '__main__':
